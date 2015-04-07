dc5320f2830d1724f9dcaf07f54f3fba2c6613eb
Luke Gaudreau <luke@infosoph.org>
2015-04-07 13:08:05 -0400

    Add a dash of bitters

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,8 @@ module.exports = function(grunt) {
 					lineNumbers: true,
 					loadPath: [
 						'bower_components/bourbon/app/assets/stylesheets',
-						'bower_components/neat/app/assets/stylesheets'
+						'bower_components/neat/app/assets/stylesheets',
+						'bower_components/bitters/app/assets/stylesheets'
 					]
 				},
 				files: {
