002c0f045bb042a15c3128ad8054c972c31d38cc
Luke Gaudreau <luke@infosoph.org>
2015-04-11 14:30:13 -0400

    Basic footer layout, typography, and colors

--- footer.php
+++ footer.php
@@ -8,18 +8,20 @@
  */
 ?>
 
-	</div><!-- #content -->
-	</div><!-- #content-wrapper -->
-	<footer id="colophon" class="site-footer" role="contentinfo">
+</div><!-- #content -->
+</div><!-- #content-wrapper -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div id="footer-wrapper" class="container">
+		<?php get_sidebar(); ?>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'reachsupply' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'reachsupply' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'reachsupply' ), 'reachsupply', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 
-			<?php get_sidebar(); ?>
 		<?php wp_nav_menu( array( 'theme_location' => 'social', 'link_before' => '<span class="screen-reader-text">', 'link_after' => '</span>' ) ); ?>
-	</footer><!-- #colophon -->
+	</div><!-- #footer-wrapper -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
