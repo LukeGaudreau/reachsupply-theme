e49de6ebeb9dbf88786053c4b2f825a942761d44
Luke Gaudreau <luke@infosoph.org>
2015-04-25 13:46:00 -0400

    Add basic layout for project list

--- content-rs_work.php
+++ content-rs_work.php
@@ -10,17 +10,16 @@
         <?php the_post_thumbnail(); ?>
         </div><!-- .entry-thumbnail -->
     <?php endif; ?>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php reachsupply_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
 	<div class="entry-content">
+        <header class="entry-header">
+            <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+            <?php if ( 'post' == get_post_type() ) : ?>
+            <div class="entry-meta">
+                <?php reachsupply_posted_on(); ?>
+            </div><!-- .entry-meta -->
+            <?php endif; ?>
+        </header><!-- .entry-header -->
         <div class="entry-excerpt">
             <?php the_excerpt(); ?>
         </div><!-- .entry-excerpt -->
