400c16704bd89ea88c16cf728e102d48083f8966
Luke Gaudreau <luke@infosoph.org>
2015-04-25 13:16:23 -0400

    Add custom template part for project list

---
+++ content-rs_work.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package reachsupply
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php if ( has_post_thumbnail() ) : ?>
+        <div class="entry-thumbnail">
+        <?php the_post_thumbnail(); ?>
+        </div><!-- .entry-thumbnail -->
+    <?php endif; ?>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php reachsupply_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+        <div class="entry-excerpt">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-excerpt -->
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'reachsupply' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php reachsupply_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
